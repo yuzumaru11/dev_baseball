809d174b0f54a06ed88b9c3cf3eb8de0ef2d37be
yuzumaru <otyapapi77@gmail.com>
2021-08-09 22:40:25 +0900

    Modified

--- ranks_insert.py
+++ ranks_insert.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[205]:
+# In[36]:
 
 
 from bs4 import BeautifulSoup
@@ -15,7 +15,7 @@ import sqlite3
 import pymysql
 
 
-# In[206]:
+# In[37]:
 
 
 def delete_ranks(table_name):
@@ -41,7 +41,57 @@ def delete_ranks(table_name):
     
 
 
-# In[207]:
+# In[38]:
+
+
+def get_team_id(team):
+    
+    team_id = ''
+    
+    if(team in '阪神') or (team in 'タイガース'):
+        team_id = 'tigers'
+        
+    elif (team in '巨人') or (team in 'ジャイアンツ'):
+        team_id = 'giants'
+    
+    elif(team in 'ヤクルト') or (team in 'スワローズ'):
+        team_id = 'swallows'
+    
+    elif(team in '中日') or (team in 'ドラゴンズ'):
+        team_id = 'dragons'
+    
+    elif(team in '広島') or (team in 'カープ'):
+        team_id = 'carp'    
+    
+    elif(team in 'DeNA') or (team in 'ベイスターズ'):
+        team_id = 'baystars'
+    
+    elif(team in 'オリックス') or (team in 'バファローズ'):
+        team_id = 'buffaloes'    
+    
+    elif(team in '楽天') or (team in 'イーグルス'):
+        team_id = 'eagles'
+    
+    elif(team in 'ロッテ') or (team in 'マリーンズ'):
+        team_id = 'marines'
+
+    elif(team in 'ソフトバンク') or (team in 'ホークス'):
+        team_id = 'hawks'
+    
+    elif(team in '西武') or (team in 'ライオンズ'):
+        team_id = 'lions'
+
+    elif(team in '日本ハム') or (team in 'ファイターズ'):
+        team_id = 'fighters'
+    
+    else:
+        team_id = ''
+    
+    return team_id
+    
+
+
+# In[39]:
 
 
 def insert_ranks(tds, table_name):
@@ -56,13 +106,13 @@ def insert_ranks(tds, table_name):
     now = datetime.datetime.now().strftime("%Y-%m-%d")
     now1 = str(datetime.datetime.now())
     baseball = 'baseball' 
-    
+    team_id = get_team_id(tds[1])
+        
     try:
         cursor = conn.cursor()
 
-        sql = "INSERT INTO " + table_name + " (day1 ,ranks1 ,team, games, win, lose, draw ,winrate ,diff ,remine_games ,score ,runs_allowed ,homerun ,steal ,hit, pitch, udate, uuser, rdate, ruser)             values(              '" + now + "'             , '" + tds[0] + "'             , '" + tds[1] + "'             , '" + tds[2] + "'             , '" + tds[3] + "'             , '" + tds[4] + "'             , '" + tds[5] + "'             , '" + tds[6] + "'             , '" + tds[7] + "'             , '" + tds[8] + "'             , '" + tds[9] + "'             , '" + tds[10] + "'             , '" + tds[11] + "'             , '" + tds[12] + "'             , '" + tds[13] + "'             , '" + tds[14] + "'             , '" + now1 + "'             , '" + baseball + "'             , '" + now1 + "'             , '" + baseball + "' )"
+        sql = "INSERT INTO " + table_name + " (day1 ,ranks1 ,team, games, win, lose, draw ,winrate ,diff ,remine_games ,score ,runs_allowed ,homerun ,steal ,hit, pitch, udate, uuser, rdate, ruser)             values(              '" + now + "'             , '" + tds[0] + "'             , '" + team_id + "'             , '" + tds[2] + "'             , '" + tds[3] + "'             , '" + tds[4] + "'             , '" + tds[5] + "'             , '" + tds[6] + "'             , '" + tds[7] + "'             , '" + tds[8] + "'             , '" + tds[9] + "'             , '" + tds[10] + "'             , '" + tds[11] + "'             , '" + tds[12] + "'             , '" + tds[13] + "'             , '" + tds[14] + "'             , '" + now1 + "'             , '" + baseball + "'             , '" + now1 + "'             , '" + baseball + "' )"
 
-        print(sql)
         cursor.execute(sql)
         conn.commit()
       
@@ -72,13 +122,7 @@ def insert_ranks(tds, table_name):
         conn.close()
 
 
-# In[ ]:
-
-
-
-
-
-# In[208]:
+# In[40]:
 
 
 url = "https://baseball.yahoo.co.jp/npb/standings/"
@@ -110,6 +154,12 @@ for i in range(len(rows)):
     if len(tds) > 0:
         insert_ranks(tds, table_name)
        
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
